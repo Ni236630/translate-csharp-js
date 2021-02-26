2adbf887144469a09cdb960650206e235f8f4a91
Nicholas Douso <nicholas.douso@gmail.com>
2021-02-26 15:16:43 -0600

    on to the next one!

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,25 @@
 // Put your code here
+
+let names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+
+names.forEach( name => {
+   return console.log(name)
+})
+
+console.log("")
+
+let theNames = []
+
+names.forEach(name => {
+    if(name.includes('The'))
+    theNames.push(name)
+});
+
+console.log("'The' Place Names")
+
+theNames.forEach(name => {
+    console.log(name)
+});
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,25 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+
+
+for (i = 0; i < 10; i++){
+    
+    let die1 = Math.floor(Math.random()*5)+1
+    
+    let die2 = Math.floor(Math.random()*5)+1
+    
+    let message = `${die1} + ${die2}  = ${die1 + die2}`
+
+   if(die1 === die2){
+        console.log( message + " Doubles ")
+   } 
+   else
+   {
+        console.log(message)
+   }
+  
+}
+
