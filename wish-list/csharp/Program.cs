023a4bea7bd4109beb892b5140d94c51c0e3bc28
nicholas douso <nicholas.douso@gmail.com>
2021-03-05 15:35:09 -0600

    wishes granted

---
+++ wish-list/csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Wishes = new List<string>();
+
+            Wishes.Add("An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'");
+            Wishes.Add("The ability to think of interesting wishes on short notice");
+            Wishes.Add("A washtub of Skittles");
+            Wishes.Add("World peace, but not the kind of world peace where all the humans are removed from existence");
+            Wishes.Add("A TV that is NOT internet-capable");
+            Wishes.Add("The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?");
+
+            Console.WriteLine("My wishes...");
+
+            foreach (string wish in Wishes)
+            {
+                Console.WriteLine(wish);
+            }
+        }
+    }
+}
